3ae2e2a931883544e2b5690caa0212dcb9738036
dakota1599 <dakotashapiro15@gmail.com>
2019-10-10 17:10:50 -0400

    Adding the add to the DroidCollection, along with its corresponding method in UI.

---
+++ cis237-assignment3/DroidCollection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237_assignment3
+{
+    class DroidCollection
+    {
+        //VARIABLES
+        private Droid[] droidArray = new Droid[100];
+        private UI ui = new UI();
+
+        //CONSTRUCTORS
+        public DroidCollection() {
+            //MAYBE I'LL PUT SOMETHING HERE, MAYBE I WON'T.  ONLY TIME WILL TELL.
+        }
+
+        //METHODS
+
+        public void Add() {
+
+        }
+    }
+}
--- cis237-assignment3/UI.cs
+++ cis237-assignment3/UI.cs
@@ -27,8 +27,12 @@ namespace cis237_assignment3
         }
 
         //THIS IS FOR INPUTTING.  IN CASE I EVER NEED TO CHANGE THE READLINE FOR SOMETHING ELSE.
-        public string Input()
+        public string Input(string words = "")
         {
+            if (words != string.Empty)
+            {
+                Output(words, false);
+            }
             string input = Console.ReadLine();
             return input;
         }
@@ -50,7 +54,7 @@ namespace cis237_assignment3
 
                 choice = Int32.Parse(Input());
 
-                if (choice >= 1 && choice <= 5) //CHECKS TO SEE IF THE CHOICE IS WITHIN THE SCOPE OF CHOICES.
+                if (choice >= 1 && choice <= 4) //CHECKS TO SEE IF THE CHOICE IS WITHIN THE SCOPE OF CHOICES.
                 {
                     return choice;
                 }
@@ -73,8 +77,72 @@ namespace cis237_assignment3
         private void PrintMenu()
         {
             Output("Please Select an Option \n----------------" + Environment.NewLine);
-            Output("1. Add Droid\n2. Print Droid List\n3. Search by ID\n4. Add Beverage\n5. Exit\n" + Environment.NewLine);
+            Output("1. Add Droid\n2. Print Droid List\n3. Search by ID\n4. Exit\n" + Environment.NewLine);
             Output("> ", false);
+        }
+
+        public Droid GettingDroidType() {
+            Output("Fill in the following information to add a droid.\n");
+            //VARS NEEDED FOR THIS METHOD
+            string mat = Input("Material Type: "); //TYPE OF MATERIAL --ALL
+            string col = Input("Color: "); //COLOR SELECTION --ALL
+            string dId = Input("Droid ID: "); //DROID ID --ALL
+            int language; //NUMBER OF LANGUAGES --PROTOCOL
+            bool tBox; //TOOLBOX --UTILITY
+            bool compCon; //COMPUTER CONNECTION --UTILITY
+            bool arm; //ARM -- UTILITY
+            bool trash; //TRASH COMPACTOR --JANITOR
+            bool vac; //VACUUM --JANITOR
+            bool fire; //FIRE EXTINQUISHER -- ASTROMECH
+            bool ships; //NUMBER OF SHIPS -- ASTROMECH
+
+            //THIS IS SO WHEN SOMEONE MAKES AN INVALID ENTRY, THEY CAN RESTART FROM THIS POINT RATHER THAN STARTING
+            //BACK AT THE BEGINNING OF THE METHOD.
+            while (true)
+            {
+                switch (Input("Droid Type: ").ToLower())
+                {
+                    case "protocol":
+                        try
+                        {
+                            language = int.Parse(Input("Number of Languages: "));
+                            return new ProtocolDroid(mat, col, dId, language);
+                        }
+                        catch (InvalidCastException ex)
+                        {
+                            Output("Must be a number");
+                            continue; //TO START OVER THE CASE STRUCTURE.
+                        }
+                    case "utility":
+                        try
+                        {
+                            Output("Please answer with true or false.\n");
+                            tBox = bool.Parse(Input("Toolbox?: "));
+                            compCon = bool.Parse(Input("Computer Connection?: "));
+                            arm = bool.Parse(Input("Arm?: "));
+                            return new UtilityDroid(mat, col, dId, tBox, compCon, arm);
+                        }
+                        catch (InvalidCastException ex) {
+                            Output("Must be true or false.");
+                            continue; //TO START OVER THE CASE STRUCTURE.
+                        }
+                    case "janitor":
+                        try {
+                            Output("Please answer with true or false.\n");
+                            tBox = bool.Parse(Input("Toolbox?: "));
+                            compCon = bool.Parse(Input("Computer Connection?: "));
+                            arm = bool.Parse(Input("Arm?: "));
+                            trash = bool.Parse(Input("Trash Compactor?: "));
+                            vac = bool.Parse(Input("Vacuum?: "));
+                            return new JanitorDroid(mat, col, dId, tBox, compCon, arm, trash, vac);
+                        }
+                        catch (InvalidCastException ex)
+                        {
+                            Output("Must be true or false.");
+                            continue; //TO START OVER THE CASE STRUCTURE.
+                        }
+                }
+            }
         }
     }
 }
